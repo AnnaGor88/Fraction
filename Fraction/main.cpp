05700650c5f0ff6d22cdb3044770b43b2673fc05
Anna <goryachevaanny@gmail.com>
2021-03-30 17:18:12 +0300

    Initial Commit

---
+++ Fraction/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Fraction
+//
+//  Created by Анна Горячева on 30.03.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
